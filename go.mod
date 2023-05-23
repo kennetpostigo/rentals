fdfcafa42b5629a0b85108661e32193595b10585
Kennet <kennet@Sofias-Air.lan>
2023-05-23 18:37:08 -0400

    install chi

---
+++ go.mod
@@ -0,0 +1,8 @@
+module rentals
+
+go 1.20
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/render v1.0.2
+)
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
-	println("HELLO")
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/rentals", getRentals)
+	router.Get("/rentals/{id}", getRental)
+
+	http.ListenAndServe(":8080", router)
 }
+
+func getRental(w http.ResponseWriter, r *http.Request) {}
+
+func getRentals(w http.ResponseWriter, r *http.Request) {}
