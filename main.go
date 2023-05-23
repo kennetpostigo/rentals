c48679602062a07fb67ae496de24c40f0e056ff9
Kennet <kennet@Sofias-Air.lan>
2023-05-23 18:40:12 -0400

    install gorm and setup connection string

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
+	dsn := "host=127.0.0.1 user=root password=root dbname=testingwithrentals port=5432"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+
+	println(db, err)
+
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
